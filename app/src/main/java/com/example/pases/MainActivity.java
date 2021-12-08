f91d007829914e7ec36014aa0f369ac6fb6ab42d
Wesley <wesleyvitor37417@gmail.com>
2021-12-08 12:30:51 -0300

    first commit

---
+++ app/src/main/java/com/example/pases/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lv = findViewById(R.id.list);
+
+
+    }
+}

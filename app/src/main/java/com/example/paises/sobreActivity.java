6249161f4ae13e9cafa036d7e5e92cd70321af35
Wesley <wesleyvitor37417@gmail.com>
2021-12-15 19:28:05 -0300

    pequena correção no título da tela de sobre

--- app/src/main/java/com/example/paises/sobreActivity.java
+++ app/src/main/java/com/example/paises/sobreActivity.java
@@ -18,13 +18,14 @@ public class sobreActivity extends AppCompatActivity {
         setSupportActionBar(topbar);
 
         if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle("");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
 
         TextView about_text = findViewById(R.id.about_text);
         String text = "Este aplicativo foi desenvolvido para fins didáticos, visando o aprendizado das tecnologias de front-end Android.\n\n" +
-                "Todos os dados e textos de informação foram retirados da WikiPédia.\n\n" +
+                "Todos os dados 000e textos de informação foram retirados da WikiPédia.\n\n" +
                 "Os ícones das bandeiras foram retiradas do endereço https://www.freepik.com/, já as demais imagens encontram-se no endereço https://www.flickr.com.\n\n" +
                 "Todas as imagens utilizadas neste projeto foram retiradas da Internet e estão sob a licença Creative Commons, disponível em: https://creativecommons.org/licenses/by/2.0/";
 
